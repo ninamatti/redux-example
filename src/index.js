6542e937db164d7a07a82bab9afd6cdf7e18f62f
Nina Matti <ninadesireematti@gmail.com>
2021-02-15 08:04:31 +0100

    Added content to two reducers and added combineReducer file as well as store import

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
+import allReducers from './reducers';
 
-
+const store = createStore(allReducers);
 
 ReactDOM.render(
   <React.StrictMode>
