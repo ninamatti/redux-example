7e03856c22749d7615d5bf1db2ac1ba0a6c05272
Nina Matti <ninadesireematti@gmail.com>
2021-02-15 10:08:03 +0100

    imported usedispatch and created onclick events

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './actions';
 
 function App() {
   const counter = useSelector( state => state.counter);
   const isLogged = useSelector( state => state.isLogged);
+  const dispatch = useDispatch();
 
   return (
     <div className="App">
       <h1>Counter {counter}</h1>
-      <button>+</button>
-      <button>-</button>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
 
       { isLogged? <h3>Info I shouldn't see!</h3> : ''}
       
