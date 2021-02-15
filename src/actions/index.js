c1bc62de12d0853d89ef78dd130deb62874c3850
Nina Matti <ninadesireematti@gmail.com>
2021-02-15 10:08:20 +0100

    added payload to increment action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,12 @@
-export const increment = () => {
+export const increment = (nr) => {
     return {
-        type: 'INCREMENT'
+        type: 'INCREMENT',
+        payload: nr
+    }
+}
+
+export const decrement = () => {
+    return {
+        type: 'DECREMENT'
     }
 }
